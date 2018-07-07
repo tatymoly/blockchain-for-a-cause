1a30564ce3ccb4221b0cc3ad810d34ae381df9e0
BloodyLink <david.quezada.m@gmail.com>
2018-07-07 19:40:00 -0400

    INITIAL_SUPPLY added to AngelToken

--- contracts/AngelToken.sol
+++ contracts/AngelToken.sol
@@ -7,4 +7,10 @@ contract AngelToken is StandardToken {
     string public constant name = "AngelToken";
     string public constant symbol = "ANG";
     uint8 public constant decimals = 18;
+    uint public INITIAL_SUPPLY = 12000;
+
+    constructor() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
 }
